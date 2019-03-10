df88b828ab45a1e4e499cf9b7224ec9c58364dfc
Tudor Timi <tudortimi@users.noreply.github.com>
2019-03-10 08:09:12 +0100

    Add script that can generate an SV package with a variable number of classes and references to other packages

---
+++ gen_package.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('name')
+parser.add_argument('--num-classes', type=int, default=1)
+parser.add_argument('--package-ref', nargs='*', default=[])
+args = parser.parse_args()
+
+with open(args.name + '.sv', 'w') as f:
+    f.write('package {};\n'.format(args.name))
+    f.write('\n')
+
+    if args.package_ref:
+        for package_ref in args.package_ref:
+            f.write('  import {}::*;\n'.format(package_ref))
+        f.write('\n')
+
+    for i in range(0, args.num_classes):
+        f.write('  class {}_class{};\n'.format(args.name, i))
+        f.write('  endclass\n')
+        f.write('\n')
+
+    f.write('endpackage\n')
